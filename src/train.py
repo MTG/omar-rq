721b23765c0cf0703c38d8bb60736820898fe29c
palonso <pablo.alonso@upf.edu>
2024-07-03 19:56:10 +0200

    Improve comments

--- src/train.py
+++ src/train.py
@@ -42,24 +42,21 @@ def train(
     module = module(net=net)
     datamodule = datamodule()
 
-    # get the lighting wandb wrapper and log the gin config
+    # get the lightning wandb logger wrapper and log the config
     gin_config_dict = gin_config_to_readable_dictionary(gin.config._OPERATIVE_CONFIG)
     wandb_logger = WandbLogger(project=project_name, save_dir=save_dir)
     wandb_logger.log_hyperparams(gin_config_dict)
 
-    # log the number of parameters in the network
+    # log the number of parameters in the network (required to compute scaling laws)
     wandb_logger.experiment.config["param_count"] = net.get_parameter_count()
 
     # create callbacks
     cosine_annealing_callback = CosineAnnealingCallback(total_steps=params["max_steps"])
     callbacks = [cosine_annealing_callback]
 
+    # create the trainer and fit the model
     trainer = Trainer(logger=wandb_logger, callbacks=callbacks, **params)
-
-    trainer.fit(
-        model=module,
-        datamodule=datamodule,
-    )
+    trainer.fit(model=module, datamodule=datamodule)
 
 
 if __name__ == "__main__":
