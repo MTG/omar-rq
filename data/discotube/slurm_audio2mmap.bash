cb0eace73af1b6fc67c3129165c77e8cacddd2fb
PRamoneda <PRamoneda@github.com>
2024-07-21 18:00:28 +0200

    new scripts slurm

--- data/discotube/slurm_audio2mmap.bash
+++ data/discotube/slurm_audio2mmap.bash
@@ -3,7 +3,7 @@
 #SBATCH --account=upf97
 #SBATCH --qos=acc_resa
 #SBATCH --cpus-per-task=1
-#SBATCH --time=15:00:00
+#SBATCH --time=30:00:00
 #SBATCH --output=debug_$1_output.txt
 #SBATCH --mail-type=all
 #SBATCH --mail-user=pedro.ramoneda@upf.edu
