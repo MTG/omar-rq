2d6e741f5a2d4674872ffffd27b6c9c9e1e03e73
raraz15 <oguza97@gmail.com>
2024-08-04 12:14:07 +0200

    build_module with ckpt path

--- src/utils.py
+++ src/utils.py
@@ -30,11 +30,20 @@ def build_module(
     representation: nn.Module,
     net: nn.Module,
     module: L.LightningModule,
+    ckpt_path: str=None,
 ):
 
+    # Evaluate the provided references, i.e. convert the strings to the actual objects
     representation = representation()
     net = net()
-    module = module(net=net, representation=representation)
+
+    if ckpt_path is not None:
+        # Load the checkpoint if provided
+        module = module.load_from_checkpoint(ckpt_path, net=net, representation=representation
+        )
+    else:
+        # Otherwise, create from random initialization
+        module = module(net=net, representation=representation)
 
     return module
 
