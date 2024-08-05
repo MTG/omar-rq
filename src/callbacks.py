cffe09d304b0a202285c69284a6ab40dab4e0414
raraz15 <oguza97@gmail.com>
2024-08-05 18:25:48 +0200

    save model config separately

--- src/callbacks.py
+++ src/callbacks.py
@@ -47,36 +47,27 @@ class GinConfigSaverCallback(Callback):
         )
         with open(model_config_path, "r") as f:
             self.model_config = f.read()
-
-        # Save the model config to ckpt_dir
+        # Create the new model config
         self.new_model_config_path = os.path.abspath(
             os.path.join(self.ckpt_dir, os.path.basename(model_config_path))
         )
-        with open(self.new_model_config_path, "w") as f:
-            f.write(self.model_config)
 
         # Remove the model config path from the train config
         self.train_config = "\n".join(train_config_lines[3:]) + "\n"
-
         # The training gin config will be saved here at the end of each epoch
         self.new_train_config_path = os.path.abspath(
             os.path.join(self.ckpt_dir, os.path.basename(self.train_config_path))
         )
 
+    # TODO: resume wandb
     def on_train_epoch_end(self, trainer, pl_module):
         """Save the gin config file in the checkpoints directory,
         appending the current checkpoint path."""
 
-        # Create the full path to the checkpoint
-        ckpt_name = f"epoch={trainer.current_epoch}-step={trainer.global_step}.ckpt"
-        ckpt_path = os.path.abspath(os.path.join(self.ckpt_dir, ckpt_name))
-
         # Update the train config with the current checkpoint path and the abspath
         # to the model config
         train_config = (
-            "# Model checkpoint path\n"
-            + f"checkpoint_path = '{ckpt_path}'\n\n"
-            + "# Load the model configuration. Do NOT change the location of these lines!\n"
+            "# Load the model configuration. Do NOT change the location of this line!\n"
             + f"include '{self.new_model_config_path}'\n\n"
             + self.train_config
         )
@@ -84,12 +75,24 @@ class GinConfigSaverCallback(Callback):
         with open(self.new_train_config_path, "w") as f:
             f.write(train_config)
 
+        # Create the full path to the checkpoint
+        ckpt_name = f"epoch={trainer.current_epoch}-step={trainer.global_step}.ckpt"
+        ckpt_path = os.path.abspath(os.path.join(self.ckpt_dir, ckpt_name))
+
+        # Update the model config with the current checkpoint path
+        model_config = (
+            "# Model checkpoint path. Do NOT change the location of this line!\n"
+            + f"build_module.ckpt_path = '{ckpt_path}'\n\n"
+            + self.model_config
+        )
+
         # If the model is a transformer, add the number of patches to the gin config
         if isinstance(pl_module.net, Transformer):
             model_config = (
-                self.model_config
+                model_config
                 + f"\nnets.transformer.Transformer.num_patches = {pl_module.net.num_patches}\n"
             )
-            # Save the updated model config
-            with open(self.new_model_config_path, "w") as f:
-                f.write(model_config)
+
+        # Save the updated model config
+        with open(self.new_model_config_path, "w") as f:
+            f.write(model_config)
