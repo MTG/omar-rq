edb54a7d6044d4aac5f8dfd56396d29d3ea13f93
Pablo Alonso <pablo.alonso@upf.edu>
2024-08-13 16:54:18 +0200

    Add cfg and slurm files

---
+++ slurm/masking_transformer_seed42.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J masking
+#SBATCH -N 1
+#SBATCH --account=upf97
+#SBATCH --partition=acc
+#SBATCH --qos=acc_resa
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=80
+#SBATCH --time=72:00:00
+#SBATCH --output=debug_%j_output.txt
+#SBATCH --mail-type=all
+#SBATCH --mail-user=pablo.alonso@upf.edu
+
+module load anaconda
+source /gpfs/projects/upf97/envs/mtg-bsc-wandb/bin/activate
+
+python3 src/train.py cfg/config_masking_transformer_seed42_resumed.gin
