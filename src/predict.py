87634d5bb7adc0eef03f41d42417fc921c29e546
raraz15 <oguza97@gmail.com>
2024-08-19 19:11:39 +0200

    improve predict

--- src/predict.py
+++ src/predict.py
@@ -1,16 +1,22 @@
 import os
+import yaml
 from argparse import ArgumentParser
 from pathlib import Path
 import traceback
 
+import numpy as np
+
+import torch
 import gin.torch
 import pytorch_lightning as L
+from torch.utils.data import DataLoader
 
 
 from data import DATASETS
 from modules import MODULES
 from nets import NETS
 from utils import gin_config_to_readable_dictionary, build_module
+from eval.dataset import AudioEmbeddingDataset
 
 # Register all modules, datasets and networs with gin
 for module_name, module in MODULES.items():
@@ -23,17 +29,37 @@ for net_name, net in NETS.items():
     gin.external_configurable(net, net_name)
 
 
-# @gin.configurable
+@torch.no_grad()
 def evaluate(
     module: L.LightningModule,
-    # datamodule: L.LightningDataModule,
-    # params: dict,
+    dataloader: DataLoader,
+    output_dir: Path,
+    # **params: dict,
 ) -> None:
     """Evaluate a trained model using the given module, datamodule and netitecture. Loads the
     weights from the checkpoint and evaluates the model on the test set."""
 
-    # disable randomness, dropout, etc...
+    precision = gin.query_parameter("train.precision")
+
+    trainer = L.Trainer(precision=precision)
+
+    # Create the output directory
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # disable randomness, dropout, melspec aug..
     module.eval()
+    module = module.to("cuda:0")  # TODO device?
+
+    for audio, audio_path in dataloader:
+        audio = audio.squeeze(0).to("cuda:0")  # (Ta, )
+        audio_name = audio_path.stem
+        output_path = output_dir / f"{audio_name}.npy"
+        # Get the embeddings
+        embeddings = module.extract_embeddings(audio)  # (L, Tc, C)
+        embeddings = embeddings.cpu().numpy()
+        # Save the embeddings
+        np.save(output_path, embeddings)  # TODO: output precision?
+        break
 
 
 if __name__ == "__main__":
@@ -44,15 +70,18 @@ if __name__ == "__main__":
         help="Path to the model config of a trained model.",
     )
     parser.add_argument(
-        "--test-config",
+        "test_config",
         type=Path,
-        default="cfg/test_config.gin",
-        help="Path to the gin config file for testing.",
+        help="Path to the config file of the downstream task's dataset.",
     )
     parser.add_argument(
         "--output-dir",
         type=Path,
+        default="./embeddings/",
         help="Path to the directory where the evaluation results will be stored.",
+    )
+    parser.add_argument(
+        "--nworkers", type=int, default=10, help="Number of workers for the dataloader."
     )
 
     args = parser.parse_args()
@@ -63,15 +92,28 @@ if __name__ == "__main__":
         # Convert the config string to a gin config
         gin.parse_config_file(args.train_config, skip_unknown=True)
 
-        # Build the module and datamodule
+        # Build the module and load the weights
         module, _ = build_module()
-        # datamodule = build_test_datamodule()
 
         gin.finalize()
 
-        evaluate(module)
+        # Load the test config
+        with open(args.test_config, "r") as f:
+            test_config = yaml.safe_load(f)
+
+        # Get the dataloader
+        dataset = AudioEmbeddingDataset(**test_config)  # TODO: gin?
+        dataloader = DataLoader(
+            dataset,
+            batch_size=1,
+            shuffle=False,
+            num_workers=args.nworkers,
+            collate_fn=dataset.collate_fn,
+        )  # TODO , pin_memory=True?
+
+        evaluate(module, dataloader, args.output_dir)
 
     except Exception:
         traceback.print_exc()
 
-    print("Evaluation completed successfully.")
+    print("Embedding extraction completed successfully.")
