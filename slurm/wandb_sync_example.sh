39d33201e1d160f74d69871a8e49c3a6d47619a5
Pablo Alonso <pablo.alonso@upf.edu>
2024-08-22 11:51:59 +0200

    Improve script

--- slurm/wandb_sync_example.sh
+++ slurm/wandb_sync_example.sh
@@ -1,14 +1,21 @@
-# Sync wandb logs from alogin1 to the current directory
+# Download logs from a server and sync wandb logs 
+#
+# Usage:
+# ./wandb_sync_example.sh
+#
+# additionally, run the script each -n seconds to keep the logs updated
+# watch -n 60 ./wandb_sync_example.sh
 
 set -e
 
 server_name=alogin1
 max_minutes=120
+folder=/gpfs/projects/upf97/logs/wandb/
 
 mkdir -p wandb/
  
 # keep a synced copy of all logs since rsync does not support filtering options
-rsync -avz $server_name:/gpfs/projects/upf97/logs/wandb/ wandb_all
+rsync -avz $server_name:$folder wandb_all
 
 # find logs that were updated less than max_minutes ago
 keep=$(find wandb_all/ -type f -mmin -$max_minutes -exec dirname {} \; | sed 's|/logs$||g' | sed 's|/files$||g' | sort | uniq)
