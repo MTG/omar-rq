e8053b3ce902fe9c0aa2aaf28e294a3f8ad3878f
raraz15 <oguza97@gmail.com>
2024-08-23 13:23:34 +0200

    downstream sh

---
+++ slurm/downstream.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J try
+#SBATCH -N 1
+#SBATCH --account=upf97
+#SBATCH --partition=acc
+#SBATCH --qos=acc_debug
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=00:60:00
+#SBATCH --output=debug_%j_output.txt
+
+module load anaconda
+source /gpfs/projects/upf97/envs/mtg-bsc/bin/activate
+
+ssl_model_id=$1
+downstream_config=$2
+
+python src/downstream.py $1 $2
