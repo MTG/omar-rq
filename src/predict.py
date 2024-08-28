5b8cf7479f9fe977c728a20acdac888083bb33b5
raraz15 <oguza97@gmail.com>
2024-08-28 16:17:46 +0200

    predict.py with gin

--- src/predict.py
+++ src/predict.py
@@ -24,8 +24,65 @@ for net_name, net in NETS.items():
     gin.external_configurable(net, net_name)
 
 
+@gin.configurable
+def define_embeddings_dir(ckpt_path: Path, dataset_name: str, root_output_dir: str):
+    """We use the following structure for the embeddings directory:
+        root_output_dir/ssl_model_id/dataset_name/
+
+    Inside dataset_name, we have the following structure:
+        dataset_name/audio_name[:3]/audio_name.pt"""
+
+    ssl_model_id = Path(ckpt_path).parent.parent.name
+    return Path(root_output_dir) / ssl_model_id / dataset_name
+
+
+@gin.configurable
+def predict(ckpt_path: Path, device_dict: dict):
+    """Wrapper function. Basically overrides some train parameters."""
+
+    train(ckpt_path=ckpt_path, device_dict=device_dict)
+
+
+@gin.configurable
+def train(
+    ckpt_path: Path,
+    params: dict,
+    device_dict: dict,
+    wandb_params=None,
+):
+    """The name is train, but we are actually predicting the embeddings for
+    the downstream task. This is done to leverage the gin config of the training.
+
+    NOTE: you have to keep wandb_params argument of this function. Otherwise,
+    Gin can not use the training config."""
+
+    # Set the output directory with model id and dataset name
+    embeddings_dir = define_embeddings_dir(ckpt_path)
+
+    # Add the callback to write the embeddings
+    callbacks = [EmbeddingWriter(embeddings_dir)]
+
+    # Overwride the device params of the training with the prediction params
+    device_dict = {**params, **device_dict}
+
+    # Create the trainer first
+    trainer = L.Trainer(callbacks=callbacks, **device_dict)
+
+    # Build the module and load the weights
+    module, _ = build_module(trainer=trainer)
+
+    # Get the data module
+    data_module = AudioEmbeddingDataModule()
+
+    # Extract embeddings with the model
+    trainer.predict(
+        module,
+        data_module,
+    )
+
+
 if __name__ == "__main__":
-    parser = ArgumentParser("Evaluate SSL models using gin config")
+    parser = ArgumentParser("Evaluate SSL models using gin configs.")
     parser.add_argument(
         "train_config",
         type=Path,
@@ -41,45 +98,19 @@ if __name__ == "__main__":
 
     try:
 
-        # Load the test config
-        with open(args.predict_config, "r") as f:
-            predict_config = yaml.safe_load(f)
-
-        # TODO: parse multiple gin configs. Or use YAML for test config
-        # Convert the config string to a gin config
-        gin.parse_config_file(args.train_config, skip_unknown=True)
-
-        # Set the output directory with model name and dataset name
-        ckpt_path = Path(gin.query_parameter("build_module.ckpt_path"))
-        ssl_model_id = ckpt_path.parent.parent.name
-        output_dir = (
-            Path(predict_config["embeddings_dir"])
-            / ssl_model_id
-            / predict_config["dataset_name"]
-        )
-
-        # Writer callback
-        callbacks = [EmbeddingWriter(output_dir)]
-
-        # Need to use a trainer for model initialization
-        trainer = L.Trainer(callbacks=callbacks, **predict_config["predict"]["device"])
-
-        # Build the module and load the weights
-        module, _ = build_module(trainer=trainer)
+        # TODO: clean this
+        # Parse the gin configs. Parse last the predict config to overwrite the train config
+        for config_file in [args.train_config, args.predict_config]:
+            gin.parse_config_file(config_file, skip_unknown=True)
 
         gin.finalize()
 
-        # Get the data module
-        audio_config = {**predict_config["predict"]["audio"]}
-        audio_config["data_dir"] = predict_config["data_dir"]
-        data_module = AudioEmbeddingDataModule(**audio_config)
+        # Get the ckpt path from the gin config
+        ckpt_path = Path(gin.query_parameter("build_module.ckpt_path"))
 
-        trainer.predict(
-            module,
-            data_module,
-        )
+        predict(ckpt_path=ckpt_path)
 
-        print("Embedding extraction completed successfully.")
+        print("Embedding extraction completed successfully!")
 
     except Exception:
         traceback.print_exc()
