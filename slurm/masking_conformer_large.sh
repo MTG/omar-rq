60aef956c5581b81fbc398a7e6e39a0f5b0a2a08
Pablo Alonso <pablo.alonso@upf.edu>
2024-09-19 18:26:30 +0200

    Fix input config

--- slurm/masking_conformer_large.sh
+++ slurm/masking_conformer_large.sh
@@ -5,7 +5,8 @@
 #SBATCH --partition=acc
 #SBATCH --qos=acc_resa
 #SBATCH --nodes=2 # This needs to match Trainer(num_nodes=...)
-#SBATCH --cpus-per-task=80
+#SBATCH --cpus-per-task=20
+#SBATCH --ntasks-per-node=4
 #SBATCH --gres=gpu:4
 #SBATCH --time=72:00:00
 #SBATCH --output=debug_%j_output.txt
@@ -13,8 +14,9 @@
 #SBATCH --mail-user=pablo.alonso@upf.edu
 
 export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
+export SRUN_NTASKS_PER_NODE=$SLURM_NTASKS_PER_NODE
 
-source /gpfs/projects/upf97/envs/mtg-bsc/bin/activate
+source /gpfs/projects/upf97/envs/mtg-bsc-codecs/bin/activate
 
 
 srun python3 src/train.py cfg/config_masking_conformer_large.gin
