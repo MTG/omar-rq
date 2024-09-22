c3fc89dc687552ffa557bceab15cd0b23feca503
Pablo Alonso <pablo.alonso@upf.edu>
2024-09-22 18:35:12 +0200

    Fix slurm scripts

--- slurm/compute_stats_conformer_cqt.sh
+++ slurm/compute_stats_conformer_cqt.sh
@@ -5,11 +5,13 @@
 #SBATCH --partition=acc
 #SBATCH --qos=acc_resa
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=40
+#SBATCH --cpus-per-task=20
 #SBATCH --time=10:00:00
 #SBATCH --output=debug_%j_output.txt
 #SBATCH --mail-type=all
 #SBATCH --mail-user=pablo.alonso@upf.edu
+
+export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
 
 source /gpfs/projects/upf97/envs/mtg-bsc/bin/activate
 
