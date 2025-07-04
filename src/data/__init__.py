7db779bc72b70269a8a66eaba6ff070dbd2d9909
Pablo Alonso <pablo.alonso@upf.edu>
2025-07-04 15:28:34 +0200

    Discar mnist dataset
    To allow for discarding torchvision dependency

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,9 +1,7 @@
-from .mnist import MNISTDataModule
 from .discotube import DiscotubeAudioDataModule, DiscotubeMultiViewAudioDataModule
 from .discotube_text_audio import DiscotubeTextAudioDataModule
 
 DATASETS = {
-    "mnist": MNISTDataModule,
     "discotube": DiscotubeAudioDataModule,
     "discotube_multiview": DiscotubeMultiViewAudioDataModule,
     "discotube_text_audio": DiscotubeTextAudioDataModule,
